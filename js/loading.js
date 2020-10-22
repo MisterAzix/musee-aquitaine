559f73624f57da7b2741a9bd7f7220006eb5d6ad
MisterAzix <flizi@live.fr>
2020-10-22 23:35:52 +0200

    Adding loading screen on every page

---
+++ js/loading.js
@@ -0,0 +1,58 @@
+let loaderContainer = document.querySelector('.loader-container');
+let loader = document.querySelector('.loader');
+let sectionLeft = document.querySelector('.loading-section-left');
+let sectionRight = document.querySelector('.loading-section-right');
+let title = document.querySelector('.loading-title');
+
+let delay = 30;
+let i = 0;
+let repeatNum = 1;
+let repeat = 1;
+
+window.addEventListener('load', loadingAnimation);
+
+//Fonction très moche mais ça marche.
+function loadingAnimation() {
+    disableScroll();
+    if(i < 48 && repeatNum <= repeat) {
+        setTimeout(() => {
+            i++;
+            loop(i);
+            console.log(i)
+            loadingAnimation();
+        }, delay);
+    }else if (repeatNum >= repeat) {
+        setTimeout(()=> openSection(), 300);
+    }else {
+        repeatNum++;
+        i = 0;
+        loadingAnimation();
+    }
+}
+
+function loop(i) {
+    loaderContainer.innerHTML += `<img class="loader" src="img/loading/${i}.png" id="${i}">`;
+    setTimeout(function () { 
+        document.getElementById(`${i}`) ? document.getElementById(`${i}`).remove() : false;
+    }, delay * 3);
+}
+
+function openSection() {
+    sectionLeft.style.width = '0';
+    sectionRight.style.width = '0';
+    title.style.display = 'none';
+    enableScroll();
+    setTimeout(() => {
+        sectionLeft.style.display = 'none';
+        sectionRight.style.display = 'none';
+        loaderContainer.style.display = 'none';
+    }, 900);
+}
+
+function disableScroll() { 
+    document.body.classList.add("stop-scrolling");
+} 
+  
+function enableScroll() { 
+    document.body.classList.remove("stop-scrolling");
+} 
